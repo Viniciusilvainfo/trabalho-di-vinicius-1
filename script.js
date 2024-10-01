7fab23c5878fd1df113a79df99795f19a1afd2cc
Vinícius Silva <47861116+Viniciusilvainfo@users.noreply.github.com>
2024-09-30 21:31:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function alteraDivPrincipal(id) {
     let campoControle = document.getElementById(id);
     let div = document.getElementById("painel-grafico");
-    
+
     switch (id) {
         case "cor-fundo":
             div.style.backgroundColor = campoControle.value;
@@ -10,13 +10,13 @@ function alteraDivPrincipal(id) {
             div.style.borderColor = campoControle.value;
             break;
         case "altura-div":
-            div.style.height = campoControle.value;
+            div.style.height = campoControle.value+"px";
             break;
         case "tamanho-fonte":
-            div.style.fontSize = campoControle.value;
+            div.style.fontSize = campoControle.value+"px";
             break;
         case "largura-div":
-            div.style.width = campoControle.value;
+            div.style.width = campoControle.value+"px";
             break;
         default:
             break;
