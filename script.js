54e3f08b181ca8b451b6693dc3b09cf06383996b
Vinícius Silva <47861116+Viniciusilvainfo@users.noreply.github.com>
2024-10-13 12:27:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function alteraDivPrincipal(id) {
+    const largura = window.innerWidth;
+
     let campoControle = document.getElementById(id);
     let div = document.getElementById("painel-grafico");
 
@@ -10,12 +12,32 @@ function alteraDivPrincipal(id) {
             div.style.borderColor = campoControle.value;
             break;
         case "altura-div":
+            if(campoControle.value < 350 || campoControle.value > 700) {
+                alert("Valor inválido para esse campo, o campo será redefinido para 400");
+                campoControle.value = 400;
+            }
+
             div.style.height = campoControle.value+"px";
             break;
         case "tamanho-fonte":
+            if(campoControle.value < 10 || campoControle.value > 22) {
+                alert("Valor inválido para esse campo, o campo será redefinido para 16");
+                campoControle.value = 16;
+            }
+
             div.style.fontSize = campoControle.value+"px";
             break;
         case "largura-div":
+            if(campoControle.value > largura) {
+                alert("O valor do campo é maior que a largura da janela, selecione um valor menor");
+                break;
+            }
+
+            if(campoControle.value < 350 || campoControle.value > 700) {
+                alert("Valor inválido para esse campo, o campo será redefinido para 400");
+                campoControle.value = 400;
+            }
+
             div.style.width = campoControle.value+"px";
             break;
         default:
@@ -23,3 +45,15 @@ function alteraDivPrincipal(id) {
     }
 
 }
+
+window.addEventListener('resize', () => {
+    const largura = window.innerWidth;
+    let campoControle = document.getElementById("largura-div");
+
+    if(campoControle.value > largura) {
+        alert("O valor do campo é maior que a largura da janela, selecione um valor menor");
+        let div = document.getElementById("painel-grafico");
+        div.style.width = "80%";
+    }
+
+});
